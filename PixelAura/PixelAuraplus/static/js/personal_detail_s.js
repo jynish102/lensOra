35fdc33c81d08ff311a233658b04851249755576
jyni22 <jaykal67kal@gmail.com>
2025-12-11 12:50:08 +0530

    Implement popup open/close functionality
    Added functionality to open and close a popup using buttons.

--- PixelAura/PixelAuraplus/static/js/personal_detail_s.js
+++ PixelAura/PixelAuraplus/static/js/personal_detail_s.js
@@ -10,3 +10,16 @@ const currentPage = window.location.pathname.split("/").pop();
       }
     }
   });
+
+/*------------------------------------------Popup-------------------------------------------------------*/
+  const openBtn = document.getElementById("openPopup");
+const closeBtn = document.getElementById("closePopup");
+const popup = document.getElementById("popup");
+
+openBtn.addEventListener("click", () => {
+    popup.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+});
