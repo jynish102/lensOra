77d66e2ea912938117eef696ab1a726a9a24e37a
jyni22 <jaykal67kal@gmail.com>
2026-01-27 14:45:10 +0530

    Update admin.py

--- PixelAura/PixelAuraplus/admin.py
+++ PixelAura/PixelAuraplus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .model import *
 
 # Register your models here.
+
+admin.site.register(Register)
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Notification)
+admin.site.register(Comment)
+admin.site.register(Follow)
+admin.site.register(Like)
