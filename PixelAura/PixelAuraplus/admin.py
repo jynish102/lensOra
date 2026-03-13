65fa20432e2b3efe8af35750f0ec9d341396e478
jyni22 <jaykal67kal@gmail.com>
2026-03-13 23:34:04 +0530

    Update admin.py

--- PixelAura/PixelAuraplus/admin.py
+++ PixelAura/PixelAuraplus/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'mobile', 'birthdate', 'username')  
+    exclude = ('password',)   # hide password field
+
+    def has_change_permission(self, request, obj=None):
+        return False   # prevent editing
+
+    def has_add_permission(self, request):
+        return False   # prevent adding
+
 class NoEditAdmin(admin.ModelAdmin):
 
     def has_change_permission(self, request, obj=None):
