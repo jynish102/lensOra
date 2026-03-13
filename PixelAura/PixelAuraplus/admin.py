c69710c8af61a4c9db8cd847c6dc4cde66ee8c1d
jyni22 <jaykal67kal@gmail.com>
2026-03-13 13:26:45 +0530

    Update admin.py

--- PixelAura/PixelAuraplus/admin.py
+++ PixelAura/PixelAuraplus/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .model import *
+from .models import *
 
 # Register your models here.
+class NoEditAdmin(admin.ModelAdmin):
 
-admin.site.register(Register)
-admin.site.register(Post)
-admin.site.register(Profile)
-admin.site.register(Notification)
-admin.site.register(Comment)
-admin.site.register(Follow)
-admin.site.register(Like)
+    def has_change_permission(self, request, obj=None):
+        return False   # prevent editing
+
+    def has_add_permission(self, request):
+        return False   # prevent adding
+
+
+admin.site.register(Register,NoEditAdmin)
+admin.site.register(Posts,NoEditAdmin)
+admin.site.register(Profile,NoEditAdmin)
+admin.site.register(Notification,NoEditAdmin)
+admin.site.register(Comment,NoEditAdmin)
+admin.site.register(Follow,NoEditAdmin)
+admin.site.register(Like,NoEditAdmin)
